c8d563942208edc5375405e53804ebfcc0f4744c
Joe Leach <joe.leach@slingshotsimulations.co.uk>
2020-11-01 21:18:02 +0000

    Initial commit
    Initial commit with basic communication from front end (VueJS) to the provided Unreal application

---
+++ unreal-pixel-streaming-app/src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home">
+    <div id="iframecontainer">
+      <iframe id="myIframe" src="http://localhost:80" :width="width" :height="height"></iframe>
+    </div>
+    <br/><br/>
+      
+    <button @click="sendMessage">Send Message</button>
+    <input type="text" v-model="jsonMessage.message">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: function() {
+    return {
+      // Storing the iFrame simply for easier access
+      iFrame: null,
+      // Json message to send to the unreal app
+      jsonMessage: {
+        type: "TestType",
+        message: "Hello world"
+      },
+    }
+  },
+  computed: {
+    width: function() {
+      // Make the iframe 50% of the window width
+      return window.innerWidth * 0.65;
+    },
+    height: function() {
+      // Calculates a rough aspect ratio of 1920 / 1080
+      return this.width / 1.778;
+    }
+  },
+  methods: {
+    sendMessage: function() {
+      // Send a json message via the iFrame
+      this.iFrame.contentWindow.postMessage(JSON.stringify(this.jsonMessage));
+    },
+    addListener: function() {
+      // Adds a listener for any messages coming from the iFrame
+      window.onmessage = function(e) {
+        console.log("Message Recieved: " + e.data);
+      }
+    }
+  },
+  mounted: function() {
+    // Pull the iFrame into data for easier access
+    this.iFrame = document.getElementById("myIframe");
+  }
+};
+</script>
+
+<style>
+#myIframe {
+  border: none;
+  border-radius: 20px;
+  box-shadow: -1px -1px 28px -6px rgba(0, 0, 0, 0.57);
+}
+</style>
